a056058ef4e7ca5c06b31c486d5653abda782559
Grant Timmerman <granttimmerman@gmail.com>
2014-06-15 23:06:07 -0700

    Made crawler simple

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 var jsdom = require('jsdom'),
   request = require('request'),
   url = require('url'),
-  npm = require("npm"),
-  redis = require("redis");
-
-var client = redis.createClient();
+  npm = require("npm");
 
 var configObject = {
   "dev": false,
@@ -45,25 +42,16 @@ function getData(j) {
   });
 }
 
+var modules = [];
+var i = 0;
+var fs = require('fs');
+
 function crawled(module) {
-  client.sadd("crawled", module, function (err, idd) {
-    if (idd == 1) {
-      npm.commands.view([module], true, function (er, data2) {
-        if (data2 != undefined) {
-          var mm = data2[Object.keys(data2)[0]];
-          if (mm != undefined) {
-            var deps = mm.dependencies;
-            var module = mm.name;
-            if (deps != undefined && Object.keys(deps).length > 0) {
-              for(var i = 0; i < Object.keys(deps).length; i++) {
-                var lib = Object.keys(deps)[i];
-                console.log(lib + " <- " + module);
-                client.sadd(lib, module);
-              }
-            }
-          }
-        }
-      });
-    }
-  });
+  modules.push(module);
+  if (i % 100 === 0) {
+    fs.writeFile("modules.txt", modules, function(err) {
+      console.log('saved!');
+    });
+  }
+  ++i;
 }
